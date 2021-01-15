b8f43a24644b10ee14a76ed9a91b8d09193ea9c5
matteoferla <matteo.ferla@gmail.com>
2021-01-15 11:09:49 +0000

    :construction: adding setup for cluster-side

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyrosetta_scripts',
+    version='0.1',
+    packages=['init_helper', 'score_mutants', 'blueprint_maker'],
+    url='https://github.com/matteoferla/pyrosetta_scripts',
+    license='MIT',
+    author='Matteo Ferla',
+    author_email='',
+    description=open('README.md').read()
+)
