15ed34c5af2d276c91799060b19bce2bbcbc2937
matteoferla <matteo.ferla@gmail.com>
2021-04-26 17:52:34 +0100

    :pencil: setup fix

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
+from warnings import warn
+from importlib import util
+import sys
+
+if sys.version_info.major != 3 or sys.version_info.minor < 6:
+    print(sys.version_info)
+    raise SystemError('Module written for Python 3.6+.')
+
+# ---------- Non pip modules  ------------------------------------------------------------------------------------------
+
+if not util.find_spec('rdkit'):
+    warn('This 3.6+ script **requires** pyrosetta, which has to be downloaded from ' +
+         'the Rosetta software site due to licencing.')
+
 from setuptools import setup, find_packages
 
 setup(
-    name='pyrosetta_scripts',
+    name='pyrosetta_help',
     version='0.1',
     packages=find_packages(),
-    url='https://github.com/matteoferla/pyrosetta_scripts',
+    url='https://github.com/matteoferla/pyrosetta_help',
     license='MIT',
     author='Matteo Ferla',
-    author_email='',
+    author_email='matteo.ferla@gmail.com',
     description=open('README.md').read()
 )
