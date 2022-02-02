4784957a1182de4f523b1490164831392a71bd30
matteoferla <matteo.ferla@gmail.com>
2022-02-02 16:18:52 +0000

    :hammer: AF2 database is at v2 now

--- pyrosetta_help/alphafold/retrieval.py
+++ pyrosetta_help/alphafold/retrieval.py
@@ -8,7 +8,7 @@ import requests
 
 
 def pose_from_alphafold2(uniprot: str) -> pyrosetta.Pose:
-    reply = requests.get(f'https://alphafold.ebi.ac.uk/files/AF-{uniprot}-F1-model_v1.pdb')
+    reply = requests.get(f'https://alphafold.ebi.ac.uk/files/AF-{uniprot}-F1-model_v2.pdb')
     assert reply.status_code == 200
     pdbblock = reply.text
     pose = pyrosetta.Pose()
@@ -26,7 +26,7 @@ def get_alphafold2_error(uniprot: str, reshaped=True) -> Union[np.ndarray, list]
     and are not pLDDT, which are stored as b-factors.
     """
     # https://alphafold.ebi.ac.uk/files/AF-Q00341-F1-predicted_aligned_error_v1.json
-    reply = requests.get(f'https://alphafold.ebi.ac.uk/files/AF-{uniprot}-F1-predicted_aligned_error_v1.json')
+    reply = requests.get(f'https://alphafold.ebi.ac.uk/files/AF-{uniprot}-F1-predicted_aligned_error_v2.json')
     assert reply.status_code == 200
     errors = reply.json()
     if reshaped:
