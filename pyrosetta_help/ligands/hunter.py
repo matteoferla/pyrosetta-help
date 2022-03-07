f78f2629cb59fe727680e05c22e239dde80e3950
matteoferla <matteo.ferla@gmail.com>
2022-03-07 11:37:24 +0000

    :beetle: typing error

--- pyrosetta_help/ligands/hunter.py
+++ pyrosetta_help/ligands/hunter.py
@@ -21,7 +21,7 @@ class LigandHunter:
     _cofactor_reference = {}
 
     @property
-    def cofactor_reference(self) -> Dict[List[dict]]:
+    def cofactor_reference(self) -> Dict[str, List[dict]]:
         if not self._cofactor_reference: #cached
             self._cofactor_reference = requests.get('https://www.ebi.ac.uk/pdbe/api/pdb/compound/cofactors').json()
         return self._cofactor_reference
